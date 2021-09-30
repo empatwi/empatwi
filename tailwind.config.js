f26408ff0a43317ec499b61e93cb103880d0e780
Letícia Vigna <let.vigna@gmail.com>
2021-09-29 23:52:31 -0300

    Created basic mobile x desktop column layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: '6A6A6A',
+        green: {
+          light: '#C3DDAD',
+          DEFAULT: '#82BC4B',
+        },
+        red: '#BC4C4B',
+      },
+      width: {
+        '48%': '48%',
+        '52%': '52%',
+      },
+    },
   },
   variants: {
     extend: {},
