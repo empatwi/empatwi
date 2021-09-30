7db1f259d9fd5bbfe6033260e9670ee432135bec
Letícia Vigna <let.vigna@gmail.com>
2021-09-29 23:58:24 -0300

    Fixed incorrect bottom border on desktop layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import './index.css';
 function App() {
   return (
     <div className="min-h-screen h-screen bg-white flex flex-col sm:flex-row">
-      <div className="border-gray border-b-2 sm:w-52% sm:border-r-2">Left</div>
+      <div className="border-gray border-b-2 sm:w-52% sm:border-b-0 sm:border-r-2">
+        Left
+      </div>
       <div className="h-full sm:w-48% bg-green-light">Right</div>
     </div>
   );
