29314d31611dc8861177d4aa884b6fe69a1d2f97
Letícia Vigna <let.vigna@gmail.com>
2021-10-02 11:51:48 -0300

    TextInput user input state

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useMemo, useState } from 'react';
 import { Text } from '../constants';
 
 type TextInputProps = {
@@ -10,13 +11,25 @@ const defaultProps = {
 };
 
 const TextInput = ({ icon, placeholder }: TextInputProps): JSX.Element => {
+  const [input, setInput] = useState('');
+
+  const textColor = useMemo(
+    () => (input ? 'text-gray' : 'text-gray-light'),
+    [input]
+  );
+
+  const handleChange = useCallback((event) => {
+    setInput(event.target.value);
+  }, []);
+
   return (
     <div className="relative flex items-center">
       <input
-        className="appearance-none w-full border-gray border-1 rounded-16px px-16px py-8px shadow text-lg text-gray-light focus focus:border-green focus:outline-none"
-        id="username"
-        type="text"
+        className={`appearance-none w-full border-gray border-1 rounded-16px px-16px py-8px shadow text-lg ${textColor} focus focus:border-green focus:outline-none`}
+        onChange={handleChange}
         placeholder={placeholder}
+        type="text"
+        value={input}
       />
       <span className="absolute leading-none right-0 pr-8px">{icon}</span>
     </div>
