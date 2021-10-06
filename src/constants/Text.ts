918eaee456271455f9b717cf5b40e103ef4ccd41
Letícia Vigna <let.vigna@gmail.com>
2021-10-05 21:46:14 -0300

    Updated Empatwi text

--- src/constants/Text.ts
+++ src/constants/Text.ts
@@ -1,6 +1,6 @@
 export enum Text {
   ASSUNTOS_DO_MOMENTO = 'assuntos do momento',
-  EMPATWI_2021 = 'Empatwi 2021',
+  EMPATWI_2021 = '© Empatwi 2021',
   O_QUE_ESTAO_PENSANDO_SOBRE = 'O que estão pensando sobre...?',
   RESULTADOS_DA_BUSCA_POR = 'resultados da busca por',
   TOTAL = 'Total',
