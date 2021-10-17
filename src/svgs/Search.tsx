97aa9d9b6a9e14791b952e8de5a1efd34e75b0f4
Letícia Vigna <let.vigna@gmail.com>
2021-10-17 10:17:51 -0300

    Set Search SVG size dimensions

--- src/svgs/Search.tsx
+++ src/svgs/Search.tsx
@@ -10,21 +10,21 @@ const defaultProps = {
 
 const Search = ({ color, size }: SearchProps): JSX.Element => {
   return (
-    <div className={`h-${size} w-${size}`}>
-      <svg
-        fill="none"
-        stroke={color}
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-        />
-      </svg>
-    </div>
+    <svg
+      fill="none"
+      height={size}
+      stroke={color}
+      viewBox="0 0 24 24"
+      width={size}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+      />
+    </svg>
   );
 };
 
