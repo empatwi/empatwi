5902be8982ed7783cb26d5098d99a03684e477bd
Letícia Vigna <let.vigna@gmail.com>
2021-10-21 19:41:57 -0300

    Mock trending data

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,418 @@ import {
 import { Logo, Search, SocialIcon } from './svgs';
 import { fetchTrendingTopics, sendSearch } from './api';
 
+const backTrending = [
+  {
+    name: 'Foden',
+    tweet_volume: 10959,
+  },
+  {
+    name: 'Ludmilla',
+    tweet_volume: 22175,
+  },
+  {
+    name: 'Ludmilla',
+    tweet_volume: 22175,
+  },
+  {
+    name: '#ONE_IN_6_MILLION',
+    tweet_volume: 177808,
+  },
+  {
+    name: '#트와이스와의_매순간이_눈부셔',
+    tweet_volume: 174247,
+  },
+  {
+    name: 'Marvel',
+    tweet_volume: 242603,
+  },
+  {
+    name: 'Marvel',
+    tweet_volume: 242603,
+  },
+  {
+    name: '#OnlyIdolJungkook',
+    tweet_volume: 93985,
+  },
+  {
+    name: '6 YEARS WITH TWICE',
+    tweet_volume: 97566,
+  },
+  {
+    name: 'BILLBOARD STAR JUNGKOOK',
+    tweet_volume: 95614,
+  },
+  {
+    name: 'Congratulations Jungkook',
+    tweet_volume: 60067,
+  },
+  {
+    name: 'Congratulations Jungkook',
+    tweet_volume: 60067,
+  },
+  {
+    name: 'Ben Simmons',
+    tweet_volume: 58200,
+  },
+  {
+    name: 'Ben Simmons',
+    tweet_volume: 58200,
+  },
+  {
+    name: 'Ben Simmons',
+    tweet_volume: 58200,
+  },
+  {
+    name: 'Cancelo',
+    tweet_volume: 11715,
+  },
+  {
+    name: 'O Globo',
+    tweet_volume: 28471,
+  },
+  {
+    name: 'PIRATA HOJE',
+    tweet_volume: 10743,
+  },
+  {
+    name: 'PIRATA HOJE',
+    tweet_volume: 10743,
+  },
+  {
+    name: 'PIRATA HOJE',
+    tweet_volume: 10743,
+  },
+  {
+    name: 'Besiktas',
+    tweet_volume: 63131,
+  },
+  {
+    name: 'Multishow',
+    tweet_volume: 25085,
+  },
+  {
+    name: '#JENNIExChanelEvent',
+    tweet_volume: 122133,
+  },
+  {
+    name: 'Thanos',
+    tweet_volume: 44310,
+  },
+  {
+    name: 'Sporting',
+    tweet_volume: 37767,
+  },
+  {
+    name: 'COCO NEIGE',
+    tweet_volume: 146151,
+  },
+  {
+    name: 'COCO NEIGE',
+    tweet_volume: 146151,
+  },
+  {
+    name: 'COCO NEIGE',
+    tweet_volume: 146151,
+  },
+  {
+    name: 'Lakers',
+    tweet_volume: 61300,
+  },
+  {
+    name: 'Lakers',
+    tweet_volume: 61300,
+  },
+  {
+    name: 'Lakers',
+    tweet_volume: 61300,
+  },
+  {
+    name: 'Lakers',
+    tweet_volume: 61300,
+  },
+  {
+    name: 'Willow',
+    tweet_volume: 14206,
+  },
+  {
+    name: 'Eternos',
+    tweet_volume: 10756,
+  },
+  {
+    name: 'Mob Psycho',
+    tweet_volume: 73396,
+  },
+  {
+    name: 'Mob Psycho',
+    tweet_volume: 73396,
+  },
+  {
+    name: 'Mob Psycho',
+    tweet_volume: 73396,
+  },
+  {
+    name: 'Mob Psycho',
+    tweet_volume: 73396,
+  },
+  {
+    name: 'Mob Psycho',
+    tweet_volume: 73396,
+  },
+  {
+    name: 'Sixers',
+    tweet_volume: 28054,
+  },
+  {
+    name: 'Sixers',
+    tweet_volume: 28054,
+  },
+  {
+    name: 'Sixers',
+    tweet_volume: 28054,
+  },
+  {
+    name: 'Sixers',
+    tweet_volume: 28054,
+  },
+  {
+    name: 'Cowboy Bebop',
+    tweet_volume: 14824,
+  },
+  {
+    name: 'Philadelphia',
+    tweet_volume: 34744,
+  },
+  {
+    name: 'Philadelphia',
+    tweet_volume: 34744,
+  },
+  {
+    name: 'Philadelphia',
+    tweet_volume: 34744,
+  },
+  {
+    name: 'Philadelphia',
+    tweet_volume: 34744,
+  },
+  {
+    name: 'Philadelphia',
+    tweet_volume: 34744,
+  },
+  {
+    name: 'Philadelphia',
+    tweet_volume: 34744,
+  },
+];
+
+const backData = {
+  positive: 46,
+  negative: 15,
+  positives_explained: [
+    {
+      word: 'vir',
+      relevance: 0.1184,
+    },
+    {
+      word: 'melhor',
+      relevance: 0.078,
+    },
+    {
+      word: 'vir',
+      relevance: 0.1283,
+    },
+    {
+      word: 'flamengo',
+      relevance: 0.1132,
+    },
+    {
+      word: 'meter',
+      relevance: 0.0579,
+    },
+    {
+      word: 'flamengo',
+      relevance: 0.1162,
+    },
+    {
+      word: 'linda',
+      relevance: 0.0628,
+    },
+    {
+      word: 'cabelo',
+      relevance: 0.1163,
+    },
+    {
+      word: 'chamar',
+      relevance: 0.0915,
+    },
+    {
+      word: 'picoliportal',
+      relevance: 0,
+    },
+    {
+      word: 'sábado',
+      relevance: 0.0618,
+    },
+    {
+      word: 'peito',
+      relevance: 0.0619,
+    },
+    {
+      word: 'ano',
+      relevance: 0.1158,
+    },
+    {
+      word: 'bom',
+      relevance: 0.2389,
+    },
+    {
+      word: 'limão',
+      relevance: 0.1104,
+    },
+    {
+      word: 'fazer',
+      relevance: 0.0734,
+    },
+    {
+      word: 'bom',
+      relevance: 0.2097,
+    },
+    {
+      word: 'bom',
+      relevance: 0.2297,
+    },
+    {
+      word: 'feliz',
+      relevance: 0.1533,
+    },
+    {
+      word: 'fla',
+      relevance: 0,
+    },
+    {
+      word: 'bom',
+      relevance: 0.2075,
+    },
+    {
+      word: 'bom',
+      relevance: 0.2394,
+    },
+    {
+      word: 'caaaaaro',
+      relevance: 0,
+    },
+    {
+      word: 'flamengo',
+      relevance: 0.1132,
+    },
+    {
+      word: 'responder',
+      relevance: 0.0466,
+    },
+    {
+      word: 'chegar',
+      relevance: 0.1609,
+    },
+    {
+      word: 'merecer',
+      relevance: 0.1173,
+    },
+    {
+      word: 'zerou',
+      relevance: 0,
+    },
+    {
+      word: 'camisa',
+      relevance: 0.0864,
+    },
+    {
+      word: 'ano',
+      relevance: 0.1091,
+    },
+    {
+      word: 'deus',
+      relevance: 0.1301,
+    },
+  ],
+  negatives_explained: [
+    {
+      word: 'bola',
+      relevance: -0.1345,
+    },
+    {
+      word: 'dever',
+      relevance: -0.1443,
+    },
+    {
+      word: 'começar',
+      relevance: -0.063,
+    },
+    {
+      word: 'ler',
+      relevance: -0.0797,
+    },
+    {
+      word: 'dia',
+      relevance: -0.2401,
+    },
+    {
+      word: 'acabar',
+      relevance: -0.1065,
+    },
+    {
+      word: 'dia',
+      relevance: -0.2529,
+    },
+    {
+      word: 'continuar',
+      relevance: -0.1068,
+    },
+    {
+      word: 'vencer',
+      relevance: -0.0762,
+    },
+    {
+      word: 'quantos',
+      relevance: -0.0797,
+    },
+    {
+      word: 'cima',
+      relevance: -0.0797,
+    },
+    {
+      word: 'começar',
+      relevance: -0.0636,
+    },
+    {
+      word: 'mundo',
+      relevance: -0.151,
+    },
+    {
+      word: 'insta',
+      relevance: -0.0769,
+    },
+    {
+      word: 'amiga',
+      relevance: -0.079,
+    },
+    {
+      word: 'dia',
+      relevance: -0.2358,
+    },
+    {
+      word: 'parar',
+      relevance: -0.0636,
+    },
+    {
+      word: 'querer',
+      relevance: -0.149,
+    },
+    {
+      word: 'ninguém',
+      relevance: -0.0786,
+    },
+  ],
+};
+
 const Empatwi = (): JSX.Element => {
   /* =====+ useState +===== */
   const [chart, setChart] = useState<GraphType | undefined>(undefined);
@@ -31,7 +443,9 @@ const Empatwi = (): JSX.Element => {
   const [isLoadingTrends, setIsLoadingTrends] = useState(true);
   const [searched, setSearched] = useState('');
   const [total, setTotal] = useState(0);
-  const [trending, setTrending] = useState<TrendingDataType[] | null>(null);
+  const [trending, setTrending] = useState<TrendingDataType[] | null>(
+    sortTrendingTopics(backTrending)
+  );
   const [wordcloud, setWordcloud] = useState<WordcloudType[] | null>(null);
 
   /* =====+ useCallback +===== */
@@ -46,7 +460,75 @@ const Empatwi = (): JSX.Element => {
         if (search) {
           setIsLoading(true);
           setSearched(search);
-          const response = await sendSearch(search);
+          // const response = await sendSearch(search);
+          const response = {
+            positive: 6,
+            negative: 9,
+            positives_explained: [
+              {
+                word: 'confirmar',
+                relevance: 0.1136,
+              },
+              {
+                word: 'dar',
+                relevance: 0.1072,
+              },
+              {
+                word: 'gostar',
+                relevance: 0.0617,
+              },
+              {
+                word: 'novo',
+                relevance: 0.1567,
+              },
+              {
+                word: 'mundinho',
+                relevance: 0.0618,
+              },
+              {
+                word: 'mds',
+                relevance: 0.0896,
+              },
+            ],
+            negatives_explained: [
+              {
+                word: 'maior',
+                relevance: -0.1692,
+              },
+              {
+                word: 'cancelar',
+                relevance: -0.1301,
+              },
+              {
+                word: 'vida',
+                relevance: -0.195,
+              },
+              {
+                word: 'roubar',
+                relevance: -0.178,
+              },
+              {
+                word: 'lindar',
+                relevance: -0.1028,
+              },
+              {
+                word: 'tendo',
+                relevance: -0.063,
+              },
+              {
+                word: 'covid',
+                relevance: -0.0797,
+              },
+              {
+                word: 'né',
+                relevance: -0.1085,
+              },
+              {
+                word: 'entender',
+                relevance: -0.266,
+              },
+            ],
+          };
           if (response) {
             // @ts-ignore
             const { chart, colors, total } = parseGraphData(response);
@@ -98,7 +580,7 @@ const Empatwi = (): JSX.Element => {
     async function fetchData() {
       const response = await fetchTrendingTopics();
       // @ts-ignore
-      if (response) setTrending(sortTrendingTopics(response));
+      // if (response) setTrending(sortTrendingTopics(response));
     }
     fetchData();
   }, []);
